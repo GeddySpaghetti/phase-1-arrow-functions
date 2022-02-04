ad3e15f826f40158147afd1dc042c09f5107a01c
geddy89 <generalgeddy@yahoo.com>
2022-02-03 20:22:06 -0800

    commit tree 83d118f712f3591d098927e46f797406239944fa

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function(num1 = 2000, num2 = 100){
+return num1 / num2
+};
+
+const square = (x = 2) => x * x;
+
+
+/*const add = (y = 3, w = 4 ) => y + w
+add(y + w);*/
+
+const add = (n1,n2) => n1+n2
